52a525b73e30bf798876f5494c254585c33b75d5
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-03-26 05:30:25 +0900

    簡易 BBS

--- main.rb
+++ main.rb
@@ -1,2 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "./bbs.db"
+)
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
+class Comment < ActiveRecord::Base
+end
+
+
+get '/' do
+  @comments = Comment.order(id: :desc).all
+  erb :index
+end
+
+post '/new' do
+  Comment.create({ body: params[:body] })
+  redirect '/'
+end
+
+post '/delete' do
+  Comment.find(params[:id]).destroy
+end
