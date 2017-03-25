9c54aac0bb3e5e5e0baf69e8ebaad6eb062697f5
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-03-26 04:52:01 +0900

    url パラメータ

---
+++ main.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+
+## url バラメータ
+
+get '/hello/:name' do
+  "hello #{params[:name]}"
+end
+get '/hello2/:fname/?:lname?' do |f, l| # 後ろに '?' をつけると optional になる
+  "hello #{f} #{l}"
+end
+
+get '/from/*/to/*' do |f, t|
+  "from #{f} to #{t}"
+end
+
+get %r{/users/([0-9]*)} do |user_id|
+  "user id = #{user_id}"
+end
+
